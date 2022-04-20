0b71238cad9baa3f6dbb23eb3e2cb83681f54d70
Dan Cropp <dcropp@amtelco.com>
2022-04-20 16:38:38 -0500

    Modified http sounds module for additional parameters, use maps for more parameters Added documentation to the module

--- lib/ex_ari/http/sounds.ex
+++ lib/ex_ari/http/sounds.ex
@@ -2,28 +2,44 @@ defmodule ARI.HTTP.Sounds do
   @moduledoc """
   HTTP Interface for CRUD operations on Sound objects
 
-  REST Reference: https://wiki.asterisk.org/wiki/display/AST/Asterisk+16+Sounds+REST+API
+  REST Reference: https://wiki.asterisk.org/wiki/display/AST/Asterisk+18+Sounds+REST+API
 
-  Sound Object: https://wiki.asterisk.org/wiki/display/AST/Asterisk+16+REST+Data+Models#Asterisk16RESTDataModels-Sound
+  Sound Object: https://wiki.asterisk.org/wiki/display/AST/Asterisk+18+REST+Data+Models#Asterisk18RESTDataModels-Sound
   """
 
   use ARI.HTTPClient, "/sounds"
   alias ARI.HTTPClient.Response
 
-  @spec list :: Response.t()
-  def list do
-    GenServer.call(__MODULE__, :list)
+  @doc """
+  Get a list of all sounds
+
+  ## Parameters
+    payload: map of the parameters and values to pass to Asterisk
+      lang: Lookup sound for a specific language
+      format: Lookup sound for a specific format
+  """
+  @spec list(map()) :: Response.t()
+  def list(payload \\ %{}) do
+    GenServer.call(__MODULE__, {:list, payload})
   end
 
+  @doc """
+  Get sound's details
+
+  ## Parameters
+    id: String (UTF-8) that represents the sound's id
+  """
   @spec get(String.t()) :: Response.t()
   def get(id) do
     GenServer.call(__MODULE__, {:get, id})
   end
 
-  def handle_call(:list, from, state) do
-    {:noreply, request("GET", "", from, state)}
+  @impl true
+  def handle_call({:list, payload}, from, state) do
+    {:noreply, request("GET", "?#{encode_params(payload)}", from, state)}
   end
 
+  @impl true
   def handle_call({:get, id}, from, state) do
     {:noreply, request("GET", "/#{id}", from, state)}
   end
